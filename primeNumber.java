468c0769fdafc19c5d83ce6c8620fe56d5f1fcba
anees <aneesk002@gmail.com>
2024-10-05 19:53:20 +0530

    primeNumberusingsqrt

---
+++ primeNumber.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+// 11. Prime  number or not
+
+// Write a Java program to check whether a given number is prime or not.
+
+class primeNumber {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter a Number : ");
+    int n = sc.nextInt();
+    
+    int flag = 0;
+    int m = n/2 ;
+    
+    if(n == 1 || n == 0){
+      System.out.print(n +" is not a prime number ");
+    } else 
+    {
+      //
+      for(int i = 2; i <= Math.sqrt(n); i++)
+        {
+          if((n % i) == 0){
+            flag = 1;
+            break;
+          }
+        }
+
+        if(flag == 0){
+          System.out.println(n + " is a prime number  ");
+        }else{
+          System.out.print(n + " is not a prime number ");
+        }
+    }
+    
+    
+    
+  }
+}
